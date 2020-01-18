cfb5c6ad59ecca922ffd47a9c92776703a89e958
LSWdigital <>
2020-01-18 15:55:10 +0000

    Bring order to src/

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './css/index.css';
+import App from './ts/App';
+import * as serviceWorker from './ts/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
